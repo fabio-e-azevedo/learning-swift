c41566c85999695af607f06ba26d1b3223027196
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2024-05-19 03:41:24 -0300

    example command line update

--- example-command-line/Sources/SwiftCommandLineApp/main.swift
+++ example-command-line/Sources/SwiftCommandLineApp/main.swift
@@ -1,7 +1,6 @@
 import Foundation
 import SwiftCommandLineAppCore
 
-// Exemplo de uso
 fetchUsers { result in
     switch result {
     case .success(let users):
@@ -12,9 +11,7 @@ fetchUsers { result in
         print("Erro: \(error)")
     }
     
-    // Encerrar o programa após a resposta
     exit(0)
 }
 
-// Manter o loop principal ativo para que a solicitação seja concluída
 RunLoop.main.run()
--- example-command-line/Sources/SwiftCommandLineAppCore/FetchUsers.swift
+++ example-command-line/Sources/SwiftCommandLineAppCore/FetchUsers.swift
@@ -1,9 +1,7 @@
 // FetchUsers.swift
 
-import Foundation
 import Alamofire
 
-// Modelo para representar um usuário
 public struct User: Codable {
     public let id: Int
     public let name: String
@@ -12,7 +10,6 @@ public struct User: Codable {
     public let status: String
 }
 
-// Função que faz a requisição e processa a resposta
 public func fetchUsers(completion: @escaping (Result<[User], AFError>) -> Void) {
     let urlString = "https://gorest.co.in/public/v2/users"
     AF.request(urlString).responseDecodable(of: [User].self) { response in
